8c13c321d68695bf0caa0819dd954793b01d146b
dtrieu728 <dtrieu7679@gmail.com>
2026-05-04 14:15:01 -0400

    Walkforward Optimizer skeleton added

--- research/walk_forward.py
+++ research/walk_forward.py
@@ -2,14 +2,21 @@
 import numpy as np
 
 class WalkForwardOptimizer:
-    def __init__(self, engine_class, strategies, data,
-                 train_window, test_window, step):
+    def __init__(self, engine_class, strategies_grid, data,
+                 train_window, test_window, step,
+                 signal_handler, execution, portfolio_factory):
+
         self.engine_class = engine_class
-        self.strategies = strategies
+        self.strategies_grid = strategies_grid
         self.data = data
+
         self.train_window = train_window
         self.test_window = test_window
         self.step = step
+
+        self.signal_handler = signal_handler
+        self.execution = execution
+        self.portfolio_factory = portfolio_factory
 
     def split(self):
         for start in range(0, len(self.data) - self.train_window - self.test_window, self.step):
@@ -17,60 +24,72 @@ class WalkForwardOptimizer:
             test = self.data.iloc[start+self.train_window:start+self.train_window+self.test_window]
             yield train, test
             
-    def optimize(self, train_data,base_strategies):
+    def optimize(self, train_data, base_strategies):
+
         best_params = {}
-        
+
         for name, strategy_class in base_strategies.items():
+
             best_sharpe = -np.inf
-            best_params = None
-            
+            best_param_for_strategy = None
+
             for params in self.strategies_grid[name]:
+
+                strategy = strategy_class(params=params)
+
                 engine = self.engine_class(
-                    data = train_data,
-                    strategies ={name:strategy},
-                    signal_handler=...,
-                    execution =...,
-                    portfolio_factory = ...
-                )
+                    data=train_data,
+                    strategies={name: strategy},
+                    signal_handler=self.signal_handler,
+                    execution=self.execution,
+                    portfolio_factory=self.portfolio_factory
+                    )
+
+                equity, _ = engine.run()
                 
-                equity,_ = engine.run()
-                sharpe = compute_sharpe(equity[name])
-                
+                returns = np.diff(equity[name])/equity[name][:-1]
+                sharpe =np.mean(returns)/ np.std(returns)
+
                 if sharpe > best_sharpe:
                     best_sharpe = sharpe
-                    best_param = params
-                    
-            best_param[name] = best_param
+                    best_param_for_strategy = params
+
+            best_params[name] = best_param_for_strategy
+
         return best_params
     
-    def test(self, test_data,best_params,base_strategies):
+    def test(self, test_data, best_params, base_strategies):
+
         strategies = {
-            name:base_strategies[name](param=best_params[name])
+            name: base_strategies[name](params=best_params[name])
             for name in base_strategies
         }
-        
+
         engine = self.engine_class(
-                    data = train_data,
-                    strategies ={name:strategy},
-                    signal_handler=...,
-                    execution =...,
-                    portfolio_factory = ...
-                )
+            data=test_data,
+            strategies=strategies,
+            signal_handler=self.signal_handler,
+            execution=self.execution,
+            portfolio_factory=self.portfolio_factory
+        )
+
         return engine.run()
     
     
-    def run(self,base_strategies):
+    def run(self, base_strategies):
+
         results = []
-        
+
         for train, test in self.split():
-            best_params = self.optimize(train,base_strategies)
-            
-            equity, turnover = self.test(test,best_params,base_strategies)
-            
+
+            best_params = self.optimize(train, base_strategies)
+
+            equity, turnover = self.test(test, best_params, base_strategies)
+
             results.append({
-                "equity":equity,
-                "turnover":turnover,
+                "equity": equity,
+                "turnover": turnover,
                 "params": best_params
             })
-        
+
         return results
