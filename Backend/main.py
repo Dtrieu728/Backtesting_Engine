aafefe8276e8483deb667aca53665de2d173d45f
Dustine Trieu <dtrieu7679@gmail.com>
2026-05-24 14:47:14 -0400

    added history page

--- Backend/main.py
+++ Backend/main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from api.backTestAPI import router
 from db.database import init_db
-from contextlib import asynccontextmanager
-
-app = FastAPI(title="Backtesting Engine")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "https://backtesting-engine-mklub5j0d-dustine-trieus-projects.vercel.app/",
-                   "https://backtesting-engine.vercel.app",],  # React dev server,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
-    
+
 app = FastAPI(title="Backtesting Engine", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://backtesting-engine-mklub5j0d-dustine-trieus-projects.vercel.app",
+        "https://backtesting-engine.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api")
